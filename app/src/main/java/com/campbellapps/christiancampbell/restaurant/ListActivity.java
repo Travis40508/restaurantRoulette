a4cba9dd4f25c4ae97e74b846eb3a7c6d6bbbeb5
Travis40508 <Rodney.Tressler@Interapthq.com>
2017-01-11 11:34:19 -0500

    Irrelevant

--- app/src/main/java/com/campbellapps/christiancampbell/restaurant/ListActivity.java
+++ app/src/main/java/com/campbellapps/christiancampbell/restaurant/ListActivity.java
@@ -25,7 +25,7 @@ public class ListActivity extends AppCompatActivity {
         Intent intent = getIntent();
         choice = intent.getStringExtra("choice");
 
-        mp = MediaPlayer.create(getApplicationContext(), R.raw.blip1);
+        mp = MediaPlayer.create(getApplicationContext(), R.raw.blip1); //irrelevant
 
         this.overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
         getExtras();
